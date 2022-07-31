8ae5f0011d433c90859840befeef5d92d81777bd
claireyurev <claire@opencortex.ai>
2022-07-30 18:45:33 -0700

    Update

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 
-const express = reqire('express')
-const app = express
+const express = require('express')
+const app = express()
 // since we will be calling this like an api, we're gonna be sending information over json
 app.use(express.json())
+// same client and server URL setup for Stripe
+app.use(express.static('public'))
 
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
@@ -11,7 +13,8 @@ const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 // so we just send item ID from the client app, and then server will pull that from a DB or some JSON file
 const storeItems = new Map([
     [1, { priceInCents: 1000, name: 'Learn React Today' }],
-    [2, { priceInCents: 2000, name: 'Learn CSS Today'}]
+    [2, { priceInCents: 2000, name: 'Learn CSS Today' }]
 ])
+
 
 app.listen(3000)
